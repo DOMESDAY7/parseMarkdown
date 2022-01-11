d71108c15f4098a3f8c46cb8e52cb5864c75b6e7
DOMESDAY <79838340+DOMESDAY7@users.noreply.github.com>
2022-01-11 21:59:14 +0100

    darkMode

--- index.php
+++ index.php
@@ -8,16 +8,21 @@
     <meta name='author' content='Domesday'>
     <meta name='description' content='description'>
     <title>RecapReseaux</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="bg-dark">
     <?php require_once("Parsedown.php") ?>
     <?php
     $parsedown = new Parsedown();
-     $text = file_get_contents("Fiche.md");
-    echo $parsedown->text($text);
-    
-    ?>
+     $text = file_get_contents("Fiche.md"); ?>
+     
+     <section class="w-75 m-auto text-white">
+         <?php
+             echo $parsedown->text($text);
+             
+             ?>
+     </section>
  
 
     
