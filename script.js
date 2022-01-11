3b9ec776b1abece02869f8bae7dcf358e41c8aab
DOMESDAY <79838340+DOMESDAY7@users.noreply.github.com>
2022-01-11 22:44:13 +0100

    bon beug chelou

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-let switch = document.querySelector(".switch");
+let switch = document.querySelector(".switch")
 let content = document.querySelector(".contentPage");
 let check = true;
-switch.addEventListener("click",)
+
+switch.addEventListener("click",changeClasses)
 
 function changeClasses(){
     if(check== true){
         document.body.classList.remove("bg-dark")
+        content.classList.remove("text-white")
+        content.classList.add("text-dark")
         document.body.classList.add("bg-white")
         check=!check;
     }else{
         document.body.classList.remove("bg-white")
         document.body.classList.add("bg-dark")
+        content.classList.add("text-white")
+        content.classList.remove("text-dark")
         check=!check;
     }
 
